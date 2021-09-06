a0db7f04767d2ae8b970209dd06c7d173543e24d
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-06 14:23:40 +0300

    pagination fix

--- src/pages/Top250Movies.js
+++ src/pages/Top250Movies.js
@@ -64,8 +64,9 @@ const Top250Movies = () => {
           <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
               <p className="text-sm text-gray-700">
-                Showing <span className="font-medium">1</span> to{" "}
-                <span className="font-medium">25</span> of{" "}
+                Showing{" "}
+                <span className="font-medium">{indexOfFirstPost + 1}</span> to{" "}
+                <span className="font-medium">{indexOfLastPost}</span> of{" "}
                 <span className="font-medium">250</span> results
               </p>
             </div>
