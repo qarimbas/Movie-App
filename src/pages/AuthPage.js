47d3cf35cf069dee9b86c31fc4adac94449af333
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-16 17:55:56 +0300

    firebase auth added

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar/NavBar";
+import React from "react";
+
+function SignUpPage() {
+  return (
+    <div>
+      <NavBar />
+      <br />
+      <center>
+        <h1 className="text-blue-500 text-5xl font-bold">Sign Up</h1>
+      </center>
+      <br />
+    </div>
+  );
+}
+
+export default SignUpPage;
