2755a994102b78316a9867fd6bf332634381f474
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-29 12:43:17 +0300

    api key change

--- src/shared/constants/imdb.constants.js
+++ src/shared/constants/imdb.constants.js
@@ -5,4 +5,5 @@ export const IMDBName_Url = "https://imdb-api.com/API/Name/";
 export const IMDBFullCast_Url = "https://imdb-api.com/API/FullCast/";
 export const IMDBInTheaters_Url = "https://imdb-api.com/en/API/InTheaters/";
 export const IMDBComingSoon_Url = "https://imdb-api.com/en/API/ComingSoon/";
-export const IMDB_Id = "k_hha1j719/";
+export const IMDB_Id_Old = "k_hha1j719/";
+export const IMDB_Id = "k_qkhwhb7k/";
